7e03d22243402f38709fb365a4dee789bf2e2aa8
Aswini2414 <tejaswini2414154@gmail.com>
2023-08-17 11:06:32 +0530

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Routes = require("./routes/route.js");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const port = process.env.port||8000;
+const port = process.env.PORT||8000;
 dotenv.config();//initializing
 app.use(cors());
 // app.use(express.json());
